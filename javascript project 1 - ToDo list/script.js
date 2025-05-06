d52575cd23df7602672c47dfc075e0f21028283f
codeher-rahavi <rahaviganeshan529@gmail.com>
2025-05-06 11:57:19 +0530

    first commit

---
+++ javascript project 1 - ToDo list/script.js
@@ -0,0 +1,39 @@
+document.querySelector('#push').onclick= function(){
+    if(document.querySelector('#newtask input').value.length === 0){
+        alert("Please Enter a task");
+        console.log("Hello");   
+    }
+    else{
+        document.querySelector('#tasks').innerHTML
+        += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="fa-solid fa-trash-can"></i>
+                </button>
+            </div>
+        
+        `;
+
+        let current_task=document.querySelectorAll(".delete");
+        for (let i=0;i<current_task.length;i++){
+            current_task[i].onclick=function(){
+                this.parentNode.remove();
+            }
+        }
+        
+
+        let task = document.querySelectorAll('.task');
+        for (let i=0;i<task.length;i++)
+        {
+            task[i].onclick=function(){
+                this.classList.toggle('completed');
+            }
+        }
+        
+        document.querySelector('#newtask input').value = "";
+
+    }
+}
