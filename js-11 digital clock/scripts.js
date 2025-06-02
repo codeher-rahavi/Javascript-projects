0c3fa3953532abf5dc12549151c205c02895f4e7
codeher-rahavi <rahaviganeshan529@gmail.com>
2025-06-02 21:40:35 +0530

    practice

---
+++ js-11 digital clock/scripts.js
@@ -0,0 +1,28 @@
+let hour = document.getElementById("hour");
+let minute = document.getElementById("minute");
+let seconds = document.getElementById("seconds");
+
+let set_Clock = setInterval(() =>{
+
+    let date_now = new Date();
+
+    let hr = date_now.getHours();
+    let min = date_now.getMinutes();
+    let sec = date_now.getSeconds();
+
+    if (hr<10){
+        hr= '0'+hr
+    }
+    if(min<10){
+        min='0'+min
+    }
+    if(sec<10){
+        sec='0'+sec
+    }
+
+    hour.textContent = hr;
+    minute.textContent = min;
+    seconds.textContent= sec;
+
+},1000);
+
