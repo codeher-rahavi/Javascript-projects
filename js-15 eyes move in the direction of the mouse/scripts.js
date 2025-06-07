dc3bc74848cf826e94eaa7dd23a1975313b7e8af
codeher-rahavi <rahaviganeshan529@gmail.com>
2025-06-07 16:16:27 +0530

    practice

---
+++ js-15 eyes move in the direction of the mouse/scripts.js
@@ -0,0 +1,48 @@
+//Selecting the eye div
+let eye_ref = document.querySelectorAll('.eye');
+
+//mousemove for the device with the mouse and touchmove for the touchscreen devices
+
+let events = ["mousemove","touchmove"]
+
+//check for touch event
+function isTouchDevice(){
+    try{
+        document.createEvent("TouchEvent");
+        return true;
+    }
+    catch(e){
+        return false;
+    }
+}
+
+//same function for both the events
+
+events.forEach((eventType) => {
+    document.body.addEventListener(eventType,
+    (event) => {
+        eye_ref.forEach((eye) => {
+            /*getBoundingclientsRect()  method returns the 
+            position relative to the viewport*/
+            let eyeX = eye.getBoundingClientRect().left +eye.clientWidth / 2;
+            let eyeY = eye.getBoundingClientRect().top +eye.clientWidth / 2;
+
+            /* clientX and clientY returm the position \
+            of the clients cursor from the top left of the screen*/
+
+            let  x=!isTouchDevice()? event.clientX : event.touches[0].clientX;
+            let y = !isTouchDevice()? event.clientY : event.touches[0].clientY;
+      
+        /*
+        subtract x position of the mouse from the x position of the eye  and 
+        y position from the y position of the eye
+        use atan(return angle in radians) */
+
+        let radian = Math.atan2( x-eyeX , y-eyeY);
+        //convert radian to degree
+        let rotationDegree = radian * (180 / Math.PI) * -1 + 180;
+        // rotate the eye
+        eye.style.transform  = "rotate(" + rotationDegree + "deg)";
+        });
+    });
+});
