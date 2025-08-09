39c83342c27ef1bd71a44d085becb40637631eb8
codeher-rahavi <rahaviganeshan529@gmail.com>
2025-08-09 22:40:06 +0530

    simple interset calculatoe

---
+++ five.simple interest calculator/scripts.js
@@ -0,0 +1,28 @@
+let calculateBtn = document.getElementById("calculate-btn");
+let result = document.getElementById('result');
+
+let calculate = () =>{
+    let p = Number(document.getElementById("principal").value);
+    let r= Number(document.getElementById("rate").value);
+    let t= Number(document.getElementById("time").value);
+    let duration = document.getElementById("duration").value;
+
+
+    console.log(p,r,t,duration);
+
+
+    let simpleInterest = duration=="year"? (p*r*t)/100 : (p*r*t)/1200;
+    console.log(simpleInterest);
+
+    let amount = p+simpleInterest;
+    console.log(amount);
+
+    result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
+    <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
+    <div>Total Amount:<span>${amount.toFixed(2)}</span></div>`;
+};
+
+ 
+
+calculateBtn.addEventListener("click",calculate);
+calculate();
